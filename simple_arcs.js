a54d795b98e669e6df006bd23e9f3f1e80bc2c05
onatb <onatbalta@yahoo.com.tr>
2016-02-23 14:28:25 +0200

    Step 2
    Create 2 arcs side by side and append different texts inside arcs

--- simple_arcs.js
+++ simple_arcs.js
@@ -5,23 +5,46 @@ var svg = d3.select("body")
     .append("g")
     .attr("transform", "translate(100,100)");
 
-var arc = d3.svg.arc()
+var arc1 = d3.svg.arc()
     .innerRadius(50)
     .outerRadius(70)
     .startAngle(0)
-    .endAngle(2 * Math.PI);
+    .endAngle(Math.PI);
 
+var arc2 = d3.svg.arc()
+    .innerRadius(50)
+    .outerRadius(70)
+    .startAngle(Math.PI)
+    .endAngle(2 * Math.PI);
+    
+//First half of the arc
 svg.append("path")
-    .attr("class", "arc")
-    .attr("id", "11")
-    .attr("d", arc);
+    .attr("class", "arc1")
+    .attr("id", "1")
+    .attr("d", arc1);
     
 svg.append("text")
-		.attr("class", "arcText")
+    .attr("class", "arcText")
     //Move the text from the start angle of the arc
-		.attr("x", 360)
+    .attr("x", 90)
     //Move the text down
-		.attr("dy", 18) 
-		.append("textPath")
-		.attr("xlink:href","#11")
-		.text("onat");	
+    .attr("dy", 18) 
+    .append("textPath")
+    .attr("xlink:href","#1")
+    .text("onat");
+
+//Other half of the arc   
+svg.append("path")
+    .attr("class", "arc2")
+    .attr("id", "2")
+    .attr("d", arc2);
+    
+svg.append("text")
+    .attr("class", "arcText")
+    //Move the text from the start angle of the arc
+    .attr("x", 90)
+    //Move the text down
+    .attr("dy", 18) 
+    .append("textPath")
+    .attr("xlink:href","#2")
+    .text("balta"); 
